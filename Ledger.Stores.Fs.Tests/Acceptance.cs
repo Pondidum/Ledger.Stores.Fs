3af76305e087deb13b949e5d05a5646fb86cb22a
andy <andy@andydote.co.uk>
2015-11-19 07:37:33 +0000

    tests: update for new acceptance testing

---
+++ Ledger.Stores.Fs.Tests/Acceptance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Ledger.Acceptance;
+
+namespace Ledger.Stores.Fs.Tests
+{
+	public class Acceptance : AcceptanceTests
+	{
+		public Acceptance() 
+			: base(new FileEventStore(GetDirectory()))
+		{
+		}
+
+		private static string GetDirectory()
+		{
+			var id = Guid.NewGuid().ToString();
+			var temp = Path.GetTempPath();
+
+			var path = Path.Combine(temp, id);
+
+			Directory.CreateDirectory(path);
+
+			return path;
+		}
+
+	}
+
+}
