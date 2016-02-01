9636250f87f010d294a4fe13c1e072dcef5c7acf
andy <andy@andydote.co.uk>
2016-02-01 09:54:24 +0000

    packages: update to support ledger 9.2.0

--- Ledger.Stores.Fs/FileStoreReader.cs
+++ Ledger.Stores.Fs/FileStoreReader.cs
@@ -22,10 +22,13 @@ namespace Ledger.Stores.Fs
 			return LoadEvents(_eventPath, aggregateID);
 		}
 
-		public IEnumerable<IDomainEvent<TKey>> LoadEventsSince(TKey aggregateID, DateTime stamp)
+		public IEnumerable<IDomainEvent<TKey>> LoadEventsSince(TKey aggregateID, DateTime? stamp)
 		{
-			return LoadEvents(aggregateID)
-				.Where(e => e.Stamp > stamp);
+			var events = LoadEvents(aggregateID);
+
+			return stamp.HasValue
+				? events.Where(e => e.Stamp > stamp)
+				: events;
 		}
 
 		public ISnapshot<TKey> LoadLatestSnapshotFor(TKey aggregateID)
